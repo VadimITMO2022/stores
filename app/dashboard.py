70cd9e2d55a190a67b0f6287499e5fee8f286577
Vadim Varshavskiy <vadim_varshavskiy@yahoo.com>
2026-02-12 03:05:21 +0300

    4th deploy

---
+++ app/dashboard.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from dash import Dash, html, dcc
+import plotly.express as px
+
+df = pd.read_csv('stores.csv', sep=',')
+
+df_number=df.groupby('Number')['Revenue'].sum().reset_index()
+fig_number = px.bar(df_number, x="Number", y="Revenue", barmode="group", title="Revenue Amounts by Store")
+
+df_city=df.groupby('City')['Revenue'].sum().reset_index()
+fig_city = px.bar(df_city, x="City", y="Revenue", barmode="group", title="Revenue Amounts by City")
+
+# Initialize the Dash app
+app = Dash(__name__)
+
+# Define the app layout
+app.layout = html.Div(children=[
+    html.H1(children='Dash Bar Chart Example'),
+    html.H1(children='This is a new deploy'),
+    html.H1(children='3-d deploy'),
+    dcc.Graph(
+        id='graph_number',
+        figure=fig_number
+    ),
+       dcc.Graph(
+        id='graph_city',
+        figure=fig_city
+    )
+])
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
